c7b3177ce7db6f3c1d99e1b8c444fbf471b1b7f7
Sasha Yusifov <yusifov200@yandex.ru>
2021-03-28 12:48:35 +0300

    Fix standalone classes

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-public class Game() {
+#include <Model.cpp>
+#include <vector>
+
+using namespace std;
+
+class Game {
     // basic parameters
     int ground_level = 19;
     int i = 130;
@@ -7,6 +12,10 @@ public class Game() {
     bool isJumping = false;
     bool isMovingUp = true;
     int game_mode = 0;
+    int score = 0;
+    int curr_dino_bitmap = 1;
+    Bitmap bDino= bDino1;
+    vector<Model> models;
 
     // jmp parameters
     int curr_h = ground_level;
@@ -14,7 +23,85 @@ public class Game() {
     int jmp_step = 1;
     
     public:
+    Game(){
+        models.push_back(Model(0,31,130,31,"ground"));
+        models.push_back(Model(0,0,&bDino1,"dino"));
+        models.push_back(Model(90,5,to_string(score),"score"));
+    }
     void on_game_tick(){
         
     }
-}
+
+    int find_model_index_by_tag(string tag){
+        int position = -1;
+        for (auto md : models){
+            position++;
+            if (md.getTag() == tag) return position;
+        }
+        return position;
+    }
+    
+    void update_dino(){
+        if(isJumping){
+            if(isMovingUp){
+                curr_h -= jmp_step;
+                if(curr_h == max_jmp_y){
+                    isMovingUp = false;
+                }
+            } else {
+                curr_h += jmp_step;
+                if (curr_h == ground_level){
+                    isJumping = false;
+                }
+            }
+        models.at(2).move_to(20,curr_h);
+        }   
+    }
+
+    void jump(){
+        if (!isJumping){
+            isJumping = true;
+            isMovingUp = true;
+            shift_dino_bm();
+            curr_h=ground_level;
+        }
+    }
+
+    void score_tick(){
+        score += 1;
+        //s+=0.005; // dynamic speed increase
+    }
+
+    void shift_dino_bm(){
+        if (!isJumping){
+            if (curr_dino_bitmap == 1){
+                // set 2nd bitmap
+                bDino = bDino2;
+                curr_dino_bitmap = 2;
+            } else {
+                // set 1st bitmap
+                bDino = bDino1;
+                curr_dino_bitmap = 1;
+            }
+        } else {
+            // jump bitmap
+            bDino = bDino3;
+            curr_dino_bitmap = 3;
+        }
+    }
+
+    void update_cactus(){
+        // update cactus position
+        //lcd.drawBitmap(i,ground_level+2,bObst.data,bObst.x,bObst.y,1);
+        i = round(i-s);
+        if (i<-16){
+            i=130;
+        }
+        
+        // detect dino collision
+        if((i>26 && i<30) && curr_h+bDino.y > ground_level) {
+            //end_game();
+        }
+    }
+
+};
---
+++ Model.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+using namespace std;
+
+static uint8_t Obst[] = {
+  0x38, 0x38, 0x20, 0xfe, 0xfe, 0x80, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x03, 0x03, 0x00, 0x00, 0x00
+};
+
+static uint8_t Dino1[] = {
+0x3c, 0x70, 0xf0, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0, 0xfe, 0xff, 0xf9, 0xf9, 0x3f, 0x2f, 0x6f, 0x0f, 
+0x0f, 0x0e, 0x00, 0x00, 0x00, 0x01, 0x07, 0x05, 0x01, 0x00, 0x0f, 0x09, 0x01, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00
+};
+
+static uint8_t Dino2[] = {
+0x3c, 0x70, 0xf0, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0, 0xfe, 0xff, 0xf9, 0xf9, 0x3f, 0x2f, 0x6f, 0x0f, 
+0x0f, 0x0e, 0x00, 0x00, 0x00, 0x01, 0x0f, 0x09, 0x01, 0x00, 0x07, 0x05, 0x01, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00
+};
+
+static uint8_t Dino3[] = {
+0x3c, 0x70, 0xf0, 0xe0, 0xe0, 0xe0, 0xe0, 0xe0, 0xfe, 0xff, 0xf9, 0xf9, 0x3f, 0x2f, 0x6f, 0x0f, 
+0x0f, 0x0e, 0x08, 0x08, 0x08, 0x09, 0x0f, 0x0d, 0x09, 0x08, 0x0f, 0x0d, 0x09, 0x08, 0x08, 0x08, 
+0x08, 0x08, 0x08, 0x08
+};
+
+static string TYPE_LINE = "line";
+static string TYPE_BITMAP = "bitmap";
+static string TYPE_TEXT = "text";
+
+
+struct Bitmap{
+    int16_t x;
+    int16_t y;
+    int Byte_in_Line;
+    uint8_t* data;
+};
+
+static Bitmap bObst = {
+  8, // XSize
+  10, // YSize
+  1, // Bytes in Line
+  Obst,  // Pointer to picture data
+};
+
+static Bitmap bDino1= {
+  18, // XSize
+  12, // YSize
+  3, // Bytes in Line
+  Dino1,  // Pointer to picture data
+};
+
+static Bitmap bDino2= {
+  18, // XSize
+  12, // YSize
+  3, // Bytes in Line
+  Dino2,  // Pointer to picture data
+};
+
+static Bitmap bDino3= {
+  18, // XSize
+  11, // YSize
+  3, // Bytes in Line
+  Dino3,  // Pointer to picture data
+};
+
+class Model {
+    int x = 0;
+    int y = 0;
+    int to_x = 0;
+    int to_y = 0;
+    string tag = "";
+    string type;
+    Bitmap* bm;
+    
+    public:
+    Model(int x, int y, Bitmap* bm, string tag = ""){
+        this->x = x;
+        this->y = y;
+        this->bm = bm;
+        this->tag = tag;
+        type = TYPE_BITMAP;
+    }
+
+    Model(int x, int y, string text, string tag = ""){
+        this->x = x;
+        this->y = y;
+        this->tag = tag;
+        type = TYPE_TEXT;
+    }
+
+    Model(int x, int y, int to_x, int to_y, string tag = ""){
+        this->x = x;
+        this->y = y;
+        this->to_x = to_x;
+        this->to_y = to_y;
+        this->tag = tag;
+        type = TYPE_LINE;
+    }
+    void move_to(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    Bitmap* getBitmap(){
+        return bm;
+    }
+    string getTag(){
+        return tag;
+    }
+    
+};
